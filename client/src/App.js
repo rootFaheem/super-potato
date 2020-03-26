3f56b4113e0bbb33ef9d7b64fd9f6f5642b4346a
rootFaheem <faheemtech1@gmail.com>
2020-03-27 01:30:03 +0530

    issue fixed

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
           <Route exact path="/" component={Fib}></Route>
           <Route exact path="/not-found" component={NotFound}></Route>
         </div>
-      </Router>
+      </Router> 
     </div>
   );
 }
