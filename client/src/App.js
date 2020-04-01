53959e4da4decd9b3f93a94b910c5c9dc29c3937
rootFaheem <faheemtech1@gmail.com>
2020-04-01 22:24:27 +0530

    restrucutring -> client and nginx

--- client/src/App.js
+++ client/src/App.js
@@ -7,28 +7,32 @@ import NotFound from "./NotFound";
 import "./App.css";
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          <h1>Fibonacci series Calculator</h1>
-          <hr></hr>
-          <Link to="/">Home</Link>
-          <br></br>
-          <br></br>
-          <Link to="/not-found">Some other page</Link>
-          <hr></hr>
-        </header>
-
+    return (
         <Router>
-          <Switch>
-            <Route exact path="/" component={Fib}></Route>
-            <Route exact path="/not-found" component={NotFound}></Route>
-          </Switch>
+            <div className="App">
+                <header className="App-header">
+                    <h1>Fibonacci series Calculator</h1>
+                    <hr></hr>
+                    <Link to="/">Home</Link>
+                    <br></br>
+                    <br></br>
+                    <Link to="/not-found">Some other page</Link>
+                    <hr></hr>
+                </header>
+
+                <Router>
+                    <Switch>
+                        <Route exact path="/" component={Fib}></Route>
+                        <Route
+                            exact
+                            path="/not-found"
+                            component={NotFound}
+                        ></Route>
+                    </Switch>
+                </Router>
+            </div>
         </Router>
-      </div>
-    </Router>
-  );
+    );
 }
 
 export default App;
--- client/src/Fib.js
+++ client/src/Fib.js
@@ -2,81 +2,83 @@ import React, { Component } from "react";
 import axios from "axios";
 
 class Fib extends Component {
-  state = {
-    seenIndexes: [],
-    values: {},
-    index: ""
-  };
+    state = {
+        seenIndexes: [],
+        values: {},
+        index: ""
+    };
 
-  componentDidMount() {
-    this.fetchValues();
-    this.fetchIndexes();
-  }
-
-  fetchValues = async () => {
-    const values = await axios.get("/api/values/current");
-
-    this.setState({
-      values: values.data
-    });
-  };
-
-  fetchIndexes = async () => {
-    const seenIndex = await axios.get("/api/values/all");
-    this.setState({
-      seenIndexes: seenIndex.data
-    });
-  };
-
-  renderSeenIndexes() {
-    return this.state.seenIndexes.map(({ number }) => number).join(", ");
-  }
-
-  renderValues() {
-    const entries = [];
-
-    for (let key in this.state.values) {
-      entries.push(
-        <div key={key}>
-          For index {key}, I calculated {this.state.values[key]}
-        </div>
-      );
+    componentDidMount() {
+        this.fetchValues();
+        this.fetchIndexes();
     }
-    return entries;
-  }
 
-  handleSubmit = async event => {
-    event.preventDefault();
+    fetchValues = async () => {
+        const values = await axios.get("/api/values/current");
 
-    await axios.post("/api/values", {
-      index: this.state.index
-    });
+        this.setState({
+            values: values.data
+        });
+    };
 
-    this.setState({ index: "" });
-  };
+    fetchIndexes = async () => {
+        const seenIndex = await axios.get("/api/values/all");
+        this.setState({
+            seenIndexes: seenIndex.data
+        });
+    };
 
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <label>Enter your index</label>
-          <input
-            value={this.state.index}
-            onChange={event => this.setState({ index: event.target.value })}
-          ></input>
-          <button>Submit</button>
-        </form>
+    renderSeenIndexes() {
+        return this.state.seenIndexes.map(({ number }) => number).join(", ");
+    }
 
-        <h3>Recently seen Indexes</h3>
-        {this.state.seenIndexes && this.state.seenIndexes[0]
-          ? this.renderSeenIndexes()
-          : null}
+    renderValues() {
+        const entries = [];
 
-        <h3>Recently seen values</h3>
-        {this.state.values ? this.renderValues() : null}
-      </div>
-    );
-  }
+        for (let key in this.state.values) {
+            entries.push(
+                <div key={key}>
+                    For index {key}, I calculated {this.state.values[key]}
+                </div>
+            );
+        }
+        return entries;
+    }
+
+    handleSubmit = async event => {
+        event.preventDefault();
+
+        await axios.post("/api/values", {
+            index: this.state.index
+        });
+
+        this.setState({ index: "" });
+    };
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>Enter your index</label>
+                    <input
+                        value={this.state.index}
+                        onChange={event =>
+                            this.setState({ index: event.target.value })
+                        }
+                    ></input>
+                    <button>Submit</button>
+                </form>
+
+                <h3>Recently seen Indexes</h3>
+                {this.state.seenIndexes && this.state.seenIndexes[0]
+                    ? this.renderSeenIndexes()
+                    : null}
+
+                <h3>Recently seen values</h3>
+                {this.state.values ? this.renderValues() : null}
+            </div>
+        );
+    }
 }
 
 export default Fib;
